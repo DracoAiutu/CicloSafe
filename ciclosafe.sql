7fd01893d58366350cd32d14c7ad7621082cac9f
Ailton <44651470+DracoAiutu@users.noreply.github.com>
2019-08-28 14:19:15 -0300

    Add files via upload

--- ciclosafe.sql
+++ ciclosafe.sql
@@ -1,90 +1,156 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 28-Ago-2019 às 19:19
+-- Versão do servidor: 10.1.26-MariaDB
+-- PHP Version: 7.1.8
 
--- -----------------------------------------------------
--- Schema ciclosafe`
--- -----------------------------------------------------
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-USE `ciclosafe` ;
 
--- -----------------------------------------------------
--- Table `cartao`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `cartao` ;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SHOW WARNINGS;
-CREATE TABLE  `cartao` (
-  `uid_card` INT NOT NULL,
-  `id_usuario` INT NOT NULL,
-  PRIMARY KEY (`uid_card`))
-ENGINE = InnoDB;
+--
+-- Database: `ciclosafe`
+--
 
-SHOW WARNINGS;
-CREATE INDEX `user_id_idx` ON `cartao` (`id_usuario` ASC);
+-- --------------------------------------------------------
 
-SHOW WARNINGS;
+--
+-- Estrutura da tabela `bike`
+--
 
--- -----------------------------------------------------
--- Table `bike`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `bike` ;
+CREATE TABLE `bike` (
+  `id` int(11) NOT NULL,
+  `marca` varchar(60) COLLATE utf8_bin NOT NULL,
+  `id_dono` int(11) NOT NULL,
+  `obs` varchar(244) COLLATE utf8_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-SHOW WARNINGS;
-CREATE TABLE  `bike` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `marca` VARCHAR(60) NOT NULL,
-  `id_dono` INT NOT NULL,
-  `obs` VARCHAR(244) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
-SHOW WARNINGS;
-CREATE INDEX `id_dono_idx` ON `bike` (`id_dono` ASC);
+--
+-- Estrutura da tabela `cartao`
+--
 
-SHOW WARNINGS;
+CREATE TABLE `cartao` (
+  `uid_card` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- -----------------------------------------------------
--- Table `usuario`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `usuario` ;
+-- --------------------------------------------------------
 
-SHOW WARNINGS;
-CREATE TABLE  `usuario` (
-  `id_usuario` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(80) NOT NULL,
-  `matricula` INT UNSIGNED NOT NULL,
-  `uid_card` INT NOT NULL,
-  `id_bike` INT NOT NULL,
-  `historico` VARCHAR(45) NULL,
-  PRIMARY KEY (`id_usuario`))
-ENGINE = InnoDB;
+--
+-- Estrutura da tabela `historico`
+--
 
-SHOW WARNINGS;
-CREATE INDEX `id_bike_idx` ON `usuario` (`id_bike` ASC);
+CREATE TABLE `historico` (
+  `id_usuario` int(11) NOT NULL,
+  `data_hora_entrada` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `data_hora_saida` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-SHOW WARNINGS;
-CREATE INDEX `uid_card_idx` ON `usuario` (`id_usuario` ASC, `uid_card` ASC);
+-- --------------------------------------------------------
 
-SHOW WARNINGS;
+--
+-- Estrutura da tabela `usuario`
+--
 
--- -----------------------------------------------------
--- Table `historico`
--- -----------------------------------------------------
-DROP TABLE `historico` ;
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nome` varchar(80) COLLATE utf8_bin NOT NULL,
+  `matricula` int(10) UNSIGNED NOT NULL,
+  `uid_card` int(11) NOT NULL,
+  `id_bike` int(11) NOT NULL,
+  `historico` varchar(45) COLLATE utf8_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-SHOW WARNINGS;
-CREATE TABLE  `historico` (
-  `id_usuario` INT NOT NULL,
-  `data_hora_entrada` TIMESTAMP NOT NULL,
-  `data_hora_saida` TIMESTAMP NOT NULL)
-ENGINE = InnoDB;
+--
+-- Extraindo dados da tabela `usuario`
+--
 
-SHOW WARNINGS;
-CREATE INDEX `usuario_id_idx` ON `historico` (`id_usuario` ASC);
+INSERT INTO `usuario` (`id_usuario`, `nome`, `matricula`, `uid_card`, `id_bike`, `historico`) VALUES
+(1, 'ailton', 1, 1, 1, NULL),
+(5, 'muito boa noite', 12, 13, 14, NULL),
+(6, 'junior', 41543566, 34563, 34, NULL);
 
-SHOW WARNINGS;
+--
+-- Indexes for dumped tables
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+--
+-- Indexes for table `bike`
+--
+ALTER TABLE `bike`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_dono_idx` (`id_dono`);
+
+--
+-- Indexes for table `cartao`
+--
+ALTER TABLE `cartao`
+  ADD PRIMARY KEY (`uid_card`),
+  ADD KEY `user_id_idx` (`id_usuario`);
+
+--
+-- Indexes for table `historico`
+--
+ALTER TABLE `historico`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD KEY `id_bike_idx` (`id_bike`),
+  ADD KEY `uid_card_idx` (`id_usuario`,`uid_card`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bike`
+--
+ALTER TABLE `bike`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `bike`
+--
+ALTER TABLE `bike`
+  ADD CONSTRAINT `id_dono` FOREIGN KEY (`id_dono`) REFERENCES `usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `cartao`
+--
+ALTER TABLE `cartao`
+  ADD CONSTRAINT `id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `historico`
+--
+ALTER TABLE `historico`
+  ADD CONSTRAINT `fk_id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
